5c95614bc606eaab7c28b1841e03294c1854d8e5
Iris Leal <iris_leal_m@hotmail.com>
2020-01-23 13:36:37 -0800

    Adding js folder

---
+++ js/js-code.js
@@ -0,0 +1,17 @@
+
+// This function ask for the country of the user and say hi in English or spanish
+function sayHi()
+{
+       var country = prompt("Which country are your visit us?","country here...");
+    if (country === "usa" || country === "USA")  {
+        greetings = 'Hello there! welcome!';
+    } else if (country === "mexico" || country === "MEXICO" || "mx") {
+        greetings = 'Hola! que gusto que nos visites!';
+    } else {
+        greetings = 'So glad you are here!';
+    }
+
+    confirm(greetings,true)
+    document.write('<h3>' + greetings + "  :-D" + '</hr>'); 
+}
+
