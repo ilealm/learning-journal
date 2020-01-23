2acf4578d162c1aeeec47b9edbd1a2799bc0652b
Iris Leal <iris_leal_m@hotmail.com>
2020-01-23 14:04:55 -0800

    Adding code

--- js/js-code.js
+++ js/js-code.js
@@ -1,8 +1,18 @@
 
+function validateEmail()
+{
+    document.write("validateEmail");
+    confirm("eMail validated",true);
+
+} //funtion validateEmail()
+
+
+
 // This function ask for the country of the user and say hi in English or spanish
 function sayHi()
 {
-       var country = prompt("Which country are your visit us?","country here...");
+    var country = prompt("Which country are your visit us?","country here...");
+
     if (country === "usa" || country === "USA")  {
         greetings = 'Hello there! welcome!';
     } else if (country === "mexico" || country === "MEXICO" || "mx") {
@@ -12,6 +22,9 @@ function sayHi()
     }
 
     confirm(greetings,true)
+    greetings = "<i>" + greetings + "</i>"
+    
     document.write('<h3>' + greetings + "  :-D" + '</hr>'); 
-}
+    
+} //function sayHi()
 
